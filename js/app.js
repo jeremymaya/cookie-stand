a471562b3b2fcf9a4edfea6cc7d0290b933bfdaf
jeremiah <jeremiah.kim@hotmail.com>
2019-08-15 04:25:12 -0700

    Rewerote the code to showcase understanding of object constructor and modular appraoch. Added event listner feature for lab-08

--- js/app.js
+++ js/app.js
@@ -1,87 +1,177 @@
 'use strict';
 
-var hourDay = ['6:00am','7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+var form = document.getElementById('new-store');
+var table = document.getElementById('sales-data');
+var tbody = document.createElement('tbody');
+var tfoot = document.createElement('tfoot');
+var tr = document.createElement('tr');
+var hour = ['6:00am','7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+var listOfStores = [];
+var listOfNewStores = [];
 
-function CookiesSold(location, minCust, maxCust, avgCookies) {
-  this.name = location;
+//Object Constructor for Stores
+function Store (location, minCust, maxCust, avgPerCust) {
+  this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
-  this.avgCookies = avgCookies;
-  CookiesSold.location.push(this.name);
-  CookiesSold.projectedSales.push(this.simulateDay());
+  this.avgPerCust = avgPerCust;
+  this.salesGenerated = this.generateSales();
+  this.dailyTotal = this.dailyTotal();
+  listOfStores.push(this);
 }
-CookiesSold.location = [];
-CookiesSold.projectedSales = [];
 
-CookiesSold.prototype.randomCustPerHour = function() {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+//Generates a random number of cookies sold based on the object properties
+Store.prototype.salesData = function () {
+  var randomGenerated = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  return Math.floor((randomGenerated * this.avgPerCust));
 };
 
-CookiesSold.prototype.simulateDay = function() {
+//Generates an array of sales data by pushing data generated from salesData prototype
+Store.prototype.generateSales = function () {
+  var sales = [];
+  for (var i = 0; i < hour.length; i++) {
+    sales.push(this.salesData());
+  }
+  return sales;
+};
+
+//Calculates daily total of cookies sold by adding all values in the array created from generateSales ptototype
+Store.prototype.dailyTotal = function () {
   var sum = 0;
-  var daySales = [];
-  for (var i = 0; i <= hourDay.length - 1; i++) {
-    var x = Math.floor(this.randomCustPerHour() * this.avgCookies);
-    sum += x;
-    daySales.push(x);
+  for (var i = 0; i < this.salesGenerated.length; i++) {
+    sum += this.salesGenerated[i];
   }
-  daySales.push(sum);
-  return daySales;
+  return sum;
 };
 
-function verticalSum() {
-  var empty = [];
-  for (var j = 0; j <= hourDay.length; j++) {
+//Creates an array of hourly total and sum of daily total
+function hourlyTotal () {
+  var totals = [];
+  for (var i = 0; i < hour.length; i++) {
     var sum = 0;
-    for (var i = 0; i <= CookiesSold.projectedSales.length - 1; i++) {
-      sum += CookiesSold.projectedSales[i][j];
+    for (var j = 0; j < listOfStores.length; j++) {
+      sum += listOfStores[j].salesGenerated[i];
     }
-    empty.push(sum);
+    totals.push(sum);
   }
-  return empty;
+  var sumTotal = 0;
+  for (var k = 0; k < totals.length; k++) {
+    sumTotal += totals[k];
+  }
+  totals.push(sumTotal);
+  return totals;
 }
 
-function prepare() {
-  var x = verticalSum();
-  x.unshift('Total');
-  CookiesSold.projectedSales.push(x);
-  hourDay.unshift('');
-  hourDay.push('Daily Location Total');
-  for (var i = 0; i <= CookiesSold.projectedSales.length - 2; i++) {
-    CookiesSold.projectedSales[i].unshift(CookiesSold.location[i]);
-  }
+//Event listener that passes user entered store information to Store object constructor
+//It prevents the page from regreshing
+//It renders a new row of sales data for the entered store
+//It should remove the old tfoot and render a new one with a updated total BUT it's not
+function newStore(event) {
+  event.preventDefault();
+  var location = event.target.location.value;
+  var minCust= parseInt(event.target.minCust.value);
+  var maxCust = parseInt(event.target.maxCust.value);
+  var avgPerCust = parseInt(event.target.avgPerCust.value);
+
+  listOfNewStores.push(new Store (location, minCust, maxCust, avgPerCust));
+
+  renderSalesTableBodyNew();
+  rednderSalesTableFootAgain();
+
+  form.reset();
+  console.log(listOfStores);
+  console.log(listOfNewStores);
 }
 
-function render() {
-  prepare();
-  var storeLocation = document.getElementById('storeLocation');
-  for (var i = 0; i <= CookiesSold.location.length + 1; i++) {
-    var table = document.createElement('tr');
-    storeLocation.appendChild(table);
+//Draws the table head
+function renderSalesTableHead () {
+  var thead = document.createElement('thead');
+  var tr = document.createElement('tr');
+  var th = document.createElement('th');
+  th.textContent = '';
+  tr.appendChild(th);
+  for (var i = 0; i < hour.length; i++) {
+    th = document.createElement('th');
+    th.textContent = hour[i];
+    tr.appendChild(th);
+  }
+  th = document.createElement('th');
+  th.textContent = 'Daily Sales Total';
+  tr.appendChild(th);
+  thead.appendChild(tr);
+  table.appendChild(thead);
+  tbody;
+}
 
-    for (var j = 0; j <= hourDay.length - 1; j++) {
-      if (i === 0) {
-        var newHour = document.createElement('th');
-        newHour.textContent = hourDay[j];
-        table.appendChild(newHour);
-      }
-      else {
-        newHour = document.createElement('td');
-        newHour.textContent = CookiesSold.projectedSales[i-1][j];
-        table.appendChild(newHour);
-      }
+//Draws the table body using the object data stored in listOfStores
+function renderSalesTableBody () {
+  for (var i = 0; i < listOfStores.length; i++){
+    var tr =document.createElement('tr');
+    var td = document.createElement('td');
+    td.textContent = listOfStores[i].location;
+    tr.appendChild(td);
+    for (var j = 0; j < hour.length; j++) {
+      td = document.createElement('td');
+      td.textContent = listOfStores[i].salesGenerated[j];
+      tr.appendChild(td);
     }
-    storeLocation.appendChild(table);
+    td = document.createElement('td');
+    td.textContent = listOfStores[i].dailyTotal;
+    tr.appendChild(td);
+    tbody.appendChild(tr);
   }
 }
 
-new CookiesSold('1st and Pike', 23, 65, 6.3);
-new CookiesSold('SeaTac Airport', 3, 24, 1.2);
-new CookiesSold('Seattle Center', 11, 38, 3.7);
-new CookiesSold('Capitol Hill', 20, 38, 2.3);
-new CookiesSold('Alki', 2, 16, 4.6);
+//Draws a row using the new object data stored in listOfNewStores
+function renderSalesTableBodyNew () {
+  for (var i = listOfNewStores.length - 1; i < listOfNewStores.length; i++){
+    var tr =document.createElement('tr');
+    var td = document.createElement('td');
+    td.textContent = listOfNewStores[i].location;
+    tr.appendChild(td);
+    for (var j = 0; j < hour.length; j++) {
+      td = document.createElement('td');
+      td.textContent = listOfNewStores[i].salesGenerated[j];
+      tr.appendChild(td);
+    }
+    td = document.createElement('td');
+    td.textContent = listOfNewStores[i].dailyTotal;
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+  }
+}
 
-console.log(CookiesSold.location);
-console.log(CookiesSold.projectedSales);
+//Draws the table foot using the array of hourly total calcualted from hourlyTotal function
+function rednerSalesTableFoot () {
+  table.appendChild(tbody);
+  tfoot;
+  tr;
+  var td = document.createElement('td');
+  td.textContent = 'Total';
+  tr.appendChild(td);
+  for (var i = 0; i < hour.length + 1; i++) {
+    td = document.createElement('td');
+    td.textContent = hourlyTotal()[i];
+    tr.appendChild(td);
+  }
+  tfoot.appendChild(tr);
+  table.appendChild(tfoot);
+}
 
-render();
+//Removes the existing table foot and draws a new table foot to disply new hourlyTotal
+function rednderSalesTableFootAgain () {
+  table.removeChild(tfoot); //Source: https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
+  rednerSalesTableFoot();
+}
+
+new Store ('1st and Pike', 23, 65, 6.3);
+new Store ('SeaTac Airport', 3, 24, 1.2);
+new Store ('Seattle Center', 11, 38, 3.7);
+new Store ('Capitol Hill', 20, 38, 2.3);
+new Store ('Alki', 2, 16, 4.6);
+
+renderSalesTableHead();
+renderSalesTableBody();
+form.addEventListener('submit', newStore);
+rednerSalesTableFoot();
+console.log(listOfStores);
